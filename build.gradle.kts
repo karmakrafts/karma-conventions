89100b5d3c49ea4c2376af29cc42723985470774
KitsuneAlex <alexh@karmakrafts.dev>
2025-03-29 18:37:44 +0100

    Update to new central.sonatype.com repositories for uploading

--- build.gradle.kts
+++ build.gradle.kts
@@ -96,8 +96,8 @@ nexusPublishing {
     repositories {
         System.getenv("OSSRH_USERNAME")?.let { userName ->
             sonatype {
-                nexusUrl = URI.create("https://s01.oss.sonatype.org/service/local")
-                snapshotRepositoryUrl = URI.create("https://s01.oss.sonatype.org/content/repositories/snapshots")
+                nexusUrl = URI.create("https://central.sonatype.com/publish/staging/maven2")
+                snapshotRepositoryUrl = URI.create("https://central.sonatype.com/repository/maven-snapshots")
                 username = userName
                 password = System.getenv("OSSRH_PASSWORD")
             }
