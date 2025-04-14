6af7775b4943f33e6fecd8b963e5b90786cf02e3
KitsuneAlex <alexh@karmakrafts.dev>
2025-04-14 18:37:29 +0200

    Add asLibrary() extension for consuming plugins as libraries

--- src/main/kotlin/dev/karmakrafts/conventions/Dependencies.kt
+++ src/main/kotlin/dev/karmakrafts/conventions/Dependencies.kt
@@ -18,6 +18,7 @@ package dev.karmakrafts.conventions
 
 import org.gradle.api.artifacts.MinimalExternalModuleDependency
 import org.gradle.api.provider.Provider
+import org.gradle.plugin.use.PluginDependency
 
 /**
  * Converts a dependency provider to an Android Archive (AAR) format.
@@ -37,4 +38,24 @@ import org.gradle.api.provider.Provider
  */
 fun Provider<MinimalExternalModuleDependency>.asAAR(): Provider<String> {
     return map { "$it@aar" }
+}
+
+/**
+ * Converts a plugin dependency provider to a library dependency format.
+ *
+ * This extension function takes a Provider of PluginDependency and
+ * transforms it into a standard Maven coordinate format that can be used
+ * as a regular library dependency in Gradle.
+ *
+ * Example usage:
+ * ```kotlin
+ * dependencies {
+ *     implementation(libs.plugins.kotlin.jvm.asLibrary())
+ * }
+ * ```
+ *
+ * @return A Provider of String representing the dependency in the format "pluginId:pluginId.gradle.plugin:version"
+ */
+fun Provider<PluginDependency>.asLibrary(): Provider<String> {
+    return map { "${it.pluginId}:${it.pluginId}.gradle.plugin:${it.version}" }
 }
