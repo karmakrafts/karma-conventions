311af8b5dfe596d59f5fe214c5d335e61f5cfd75
KitsuneAlex <alexh@karmakrafts.dev>
2025-05-24 02:46:18 +0200

    1.3.13 - Fix wrong import for defaultDependencyLocking()

--- src/main/kotlin/dev/karmakrafts/conventions/DependencyLocking.kt
+++ src/main/kotlin/dev/karmakrafts/conventions/DependencyLocking.kt
@@ -16,8 +16,8 @@
 
 package dev.karmakrafts.conventions
 
-import com.android.build.gradle.internal.tasks.DependencyReportTask
 import org.gradle.api.Project
+import org.gradle.api.tasks.diagnostics.DependencyReportTask
 import org.gradle.kotlin.dsl.register
 
 /**
